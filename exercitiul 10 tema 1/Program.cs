dd1b947b291dc4d7b3f88887baa7dee1fdbf1347
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-01 23:57:18 +0300

    Add project files.

---
+++ exercitiul 10 tema 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+            int cifra = 0;
+            int suma = 0;
+
+            while (n != 0)
+            {
+                cifra = n % 10;
+                suma = cifra + suma;
+                n = n /= 10;
+
+            }
+            Console.Write(suma);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
